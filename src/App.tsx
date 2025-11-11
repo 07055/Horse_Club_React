bf3bde4a1570067e003c22a4f52ff10f473e85cf
unknown <brianzioki100@gmail.com>
2025-11-11 16:32:00 +0300

    my first react project

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import Header from './components/Header.tsx'
+import Hero from './components/Hero.tsx'
+import './App.css'
+
+function App() {
+  return (
+    <>
+     <Header/>
+     <Hero/>
+
+    </>
+    
+  )
+}
+
+export default App
